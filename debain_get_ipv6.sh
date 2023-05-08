b7d33c0882a56147b774ec4fd6cc744ff7e82c64
PAINCLOWN <canyoing@gmail.com>
2023-05-08 22:42:15 +0800

    Update debain_get_ipv6.sh

--- debain_get_ipv6.sh
+++ debain_get_ipv6.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
 echo "Author:PIANCLOWN"
-
 nmcli c del br0 wifi wlan0 usb0 usb USB bridge usb-share 
-
 nmcli c add type bridge ifname br0 con-name br0 ipv4.method manual ipv4.addresses 192.168.68.1/24 ipv4.dns '223.5.5.5'
-
 nmcli c add type ethernet ifname usb0 con-name usb
-
 nmcli c add type  wifi  ifname wlan0 con-name wlan0   ssid @sudo_LTE mode ap master br0
-
 nmcli c add type bridge-slave ifname usb0 con-name usb0 master br0
-
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
@@ -27,15 +22,10 @@ systemctl start dnsmasq.service
 nmcli c modify br0 ipv6.method ignore
 nmcli c modify wlan0 wifi-sec.key-mgmt wpa-psk
 nmcli c modify wlan0 wifi-sec.psk "741852abc"
-
 echo "sleep 10"
 sleep 10
-
 echo "go on"
-
-
 cat >/etc/apt/sources.list<<EOF
-# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb http://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
 deb-src http://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
 
@@ -51,19 +41,14 @@ deb-src http://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security ma
 # deb http://security.debian.org/debian-security bullseye-security main contrib non-free
 # deb-src http://security.debian.org/debian-security bullseye-security main contrib non-free
 EOF
-
-
 apt-get  -y  update
 apt-get -y install ndppd radvd wide-dhcpv6-server iproute2
-
 cat>/etc/NetworkManager/dispatcher.d/ipv6-conf.sh<<EOF
 #!/usr/bin/env bash
-
 interface=\$1
 event=\$2
 echo "\$interface" >> /tmp/ipv6-conf.log
 if [ "\$interface" == "wwan0" ]; then
-        
         prefix=\$(ip -6 addr show dev wwan0| sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d'|head -n 1|cut -f'1-4' -d':')
         # just assume prefix length /64 here
         echo "get prefix \$prefix::/64"|systemd-cat -t ipv6_conf
@@ -88,9 +73,7 @@ if [ "\$interface" == "wwan0" ]; then
         echo "ok" >> /tmp/ipv6-conf.log
 fi
 EOF
-
 chmod 755 /etc/NetworkManager/dispatcher.d/ipv6-conf.sh
-
 cat >/etc/ndppd.conf.bak<<EOF
 proxy wwan0 {
     router yes
@@ -101,7 +84,6 @@ auto
 }
 }
 EOF
-
 cat >/etc/radvd.conf.bak<<EOF
 interface br0 {
         AdvSendAdvert on;
@@ -116,17 +98,12 @@ interface br0 {
         };
 };
 EOF
-
 cat >/etc/wide-dhcpv6/dhcp6s.conf.bak<<EOF
 interface br0 {
         address-pool pool1 3600;
 };
-
 pool pool1 {
         range PREFIX_MIN to PREFIX_MAX ;
 };
 EOF
-
 echo "end"
-
-reboot
